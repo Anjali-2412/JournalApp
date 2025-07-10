884ea542b1785be948f30e83e9ee40311685822d
Anjali-2412 <naina.jain3333@gmail.com>
2025-07-10 16:28:41 +0530

    get all Entry method has created for Journal Entry App with HTTp status code

--- journalApp/src/main/java/net/app/journalApp/controller/JournalEntryV2Controller.java
+++ journalApp/src/main/java/net/app/journalApp/controller/JournalEntryV2Controller.java
@@ -4,11 +4,14 @@ import net.app.journalApp.entity.JournalEntry;
 import net.app.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/journal")
@@ -33,8 +36,13 @@ public class JournalEntryV2Controller {
 
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
-        return journalEntryService.findById(myId).orElse(null);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId){
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+        //return journalEntry.map(entry -> new ResponseEntity<>(entry, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if(journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
@@ -43,8 +51,14 @@ public class JournalEntryV2Controller {
         return true;
     }
     @PutMapping("update/{myId}")
-    public boolean updateJournalEntryById(@PathVariable ObjectId myId,@RequestBody JournalEntry myEntry){
+    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId,@RequestBody JournalEntry newEntry){
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
+        if(old!=null){
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle(): old.getTitle() );
+            old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent()  );
 
-        return true;
+        }
+        journalEntryService.saveEntry(old);
+        return newEntry;
     }
 }
